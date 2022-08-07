b7985eda99bb0b488a15aba05cd2817c579268ab
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-08-07 18:56:32 +0200

    add origin localhost - CORS

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "https://jamelfase.com",
+  origin: ["http://localhost:3000", "https://jamelfase.com"],
   credentials: true,
   allowedHeaders: ["sessionId", "Content-Type"],
   exposedHeaders: ["sessionId"],
