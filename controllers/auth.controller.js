9c805e645e764f6ccd95a273f491ed43a560e177
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-08-21 22:44:18 +0200

    m

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -59,7 +59,7 @@ module.exports.signIn = async (req, res) => {
       sameSite: true,
     });
     res.cookie("jwt", token, { httpOnly: true, maxAge, secure: true });
-    res.status(200).json({ message: "Vous etes connecté !" });
+    res.status(200).json({ message: "Vous etes connecté !", userid: "Bienvenue sur jamelfase" });
   } catch (error) {
     console.log(error.message);
   }
