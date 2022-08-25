a1c0fa141a5dda38dd2f66bc75c0f1fdc831ff36
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-08-25 23:53:02 +0200

    m

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ const corsOptions = {
   origin: ["https://jamelfase.com"],
   optionsSuccessStatus: 200,
 };
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 //routes
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
-app.use("/api/user", userRoute, cors(corsOptions));
+app.use("/api/user", userRoute);
 
 // server
 app.listen(process.env.PORT, () => {
